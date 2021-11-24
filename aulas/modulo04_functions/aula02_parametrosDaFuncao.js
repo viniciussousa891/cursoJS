b22e436c2dba1458b9fff4166cc7a8103460f3c3
Vinicius de Sousa Carvalho <sousav387@gmail.com>
2021-11-24 15:55:46 -0300

    modulo04_aula02

--- aulas/modulo04_functions/aula02_parametrosDaFuncao.js
+++ aulas/modulo04_functions/aula02_parametrosDaFuncao.js
@@ -31,3 +31,40 @@ function soma(){
 
 let somatorio = soma(1,2,3,4,5,6,7,8,9,10);
 console.log(`soma = ${somatorio}`);
+
+
+
+/* Paarametro pre-definido */
+
+/* Dessa forma, caso o valor de b não seja enviado, 
+a conta ainda será executada */
+function soma02(a, b = 0){
+    console.log(a  + b);
+}
+
+soma02(1);
+
+
+/* Atribuição via desestruturação (tanto para objeto quanto para array)*/
+function soma03({nome, sobrenome, idade}){
+    console.log(nome, sobrenome, idade);
+}
+
+soma03({nome: "Vincius", sobrenome: "Carvalho", idade: 19});
+
+
+/* Usando rest operator */
+/* com ele, é possivel armazenar em um array todos 
+os outros valores digitados */
+
+function soma04(operador, acumulador, ...numeros){
+    for(let numero of numeros){
+        if (operador === "+") acumulador += numero;
+        if (operador === "-") acumulador -= numero;
+        if (operador === "/") acumulador /= numero;
+        if (operador === "*") acumulador *= numero;
+    }
+    console.log(acumulador);
+}
+
+soma04("-", 1000,1,5,6,7,84,5,4);
